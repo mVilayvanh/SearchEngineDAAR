0178b1b8cceb2c557c048027588e8b9cc1141570
mVilayvanh <mickaelvilayvanh@gmail.com>
2025-11-10 11:01:03 +0100

    init

---
+++ SeachEngineAPI/src/main/java/com/daar/SeachEngineAPI/SeachEngineApiApplication.java
@@ -0,0 +1,13 @@
+package com.daar.SeachEngineAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeachEngineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeachEngineApiApplication.class, args);
+	}
+
+}
---
+++ SeachEngineAPI/src/test/java/com/daar/SeachEngineAPI/SeachEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daar.SeachEngineAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeachEngineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
