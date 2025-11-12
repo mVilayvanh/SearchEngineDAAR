4c21dfa470efee0efc528aff957382799e98bbe8
Mickael VILAYVANH <mickaelvilayvanh@gmail.com>
2025-11-12 12:37:08 +0100

    update: lombok annotation for entity + renaming package

--- SeachEngineAPI/src/main/java/com/daar/SeachEngineAPI/controllers/BasicSearchController.java
+++ SeachEngineAPI/src/main/java/com/daar/SeachEngineAPI/controllers/BasicSearchController.java
@@ -2,9 +2,7 @@ package com.daar.SeachEngineAPI.controllers;
 
 import com.daar.SeachEngineAPI.entity.Book;
 import com.daar.SeachEngineAPI.service.BasicSearchService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,10 +11,11 @@ import java.util.List;
 public class BasicSearchController {
     private final BasicSearchService bss;
 
-    public  BasicSearchController(BasicSearchService basicSearchService) {
+    public BasicSearchController(BasicSearchService basicSearchService) {
         this.bss = basicSearchService;
     }
 
+    @GetMapping
     public List<Book> search(@RequestParam String query) {
         return this.bss.searchBook(query);
     }
--- SeachEngineAPI/src/main/java/com/daar/SeachEngineAPI/entity/Book.java
+++ SeachEngineAPI/src/main/java/com/daar/SeachEngineAPI/entity/Book.java
@@ -5,9 +5,13 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "books")
+@Getter
+@Setter
 public class Book {
 
     @Id
@@ -17,15 +21,13 @@ public class Book {
     @Column(name = "title",  nullable = false)
     private String title;
 
+    @Column(name = "author", nullable = false)
+    private String author;
+
     @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
 }
